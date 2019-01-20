524576fe8a5f8b36ca6ac03f4d22a81955a0d428
Guilherme Silva Paim <guisilvapaim@gmail.com>
2019-01-20 21:20:34 -0200

    Implementações dos componentes e API / Components and API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
 import GlobalStyle from './styles/global';
 
-const Title = styled.h1`
-  color: white;
-  font-size: 32px;
-`;
+import Home from './pages/Home';
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <Home />
   </Fragment>
 );
 
